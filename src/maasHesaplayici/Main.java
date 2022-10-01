911104af894394703ce1cf228eb37e32bcbdbd81
huseyin <hsyn_hsyn77@hotmail.com>
2022-10-01 19:18:56 +0300

    Initial commit

---
+++ src/maasHesaplayici/Main.java
@@ -0,0 +1,12 @@
+package maasHesaplayici;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee e1 = new Employee("Ali", 2000, 45, 2010);
+        Employee e2 = new Employee("Kaan", 14000, 40, 2020);
+        Employee e3 = new Employee("Hüseyin", 22000, 26, 2001);
+        e1.toString1();
+        e2.toString1();
+        e3.toString1();
+    }
+}
